efa935e0a656b21e566c7b97eec6087b94b18046
fxxwol <protsivroksolana2004@gmail.com>
2023-07-02 17:34:21 +0300

    fixed

--- src/components/Contacts/ContactList.jsx
+++ src/components/Contacts/ContactList.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import ContactItem from './ContactItem';
-import { ListItem } from '@mui/material';
 import { List, Item} from './ContactList.styled';
 
 const ContactList = ({ contacts, onDelete }) => {
